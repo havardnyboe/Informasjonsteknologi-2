3da1ee106d3415d8df5db4ee2b4d8b86ee502345
Håvard Nybøe <havard.nyboe@gmail.com>
2020-05-05 14:59:25 +0200

    Create script.js

---
+++ 2019/35/Opperatorer - Oppgave/script.js
@@ -0,0 +1,8 @@
+var tall1 = 3;  //deklaerer variablenen tall1 og tillordner den verdien 3
+var tall2 = 4;  //deklaerer variablenen tall2 og tillordner den verdien 4
+
+console.log("tall1 er", tall1);
+console.log("tall2 er", tall2);
+console.log("Summen blir", tall1 + tall2);
+console.log("Differansen blir", tall1 - tall2);
+console.log("Produktet blir", tall1 * tall2);
