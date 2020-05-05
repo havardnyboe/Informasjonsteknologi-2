611b6e44cb70a493ee04b69ae28688726a1c2b4f
Håvard Nybøe <havard.nyboe@gmail.com>
2020-05-05 15:01:44 +0200

    Create pizzaOgDrikke.js

---
+++ 2020/11/PizzaMeny/pizzaOgDrikke.js
@@ -0,0 +1,11 @@
+// Pizzaer
+
+let margherita = new Pizza("Margherita", ["Hvetemel", "Egg", "Vann", "Tomatsaus", "Mozzarellaost"], 99);
+let pepperoni = new Pizza("Pepperoni", ["Hvetemel", "Egg", "Vann", "Tomatsaus", "Mozzarellaost", "Peperoni", "Oregano"], 119);
+let ostOgBacon = new Pizza("Ost & Bacon", ["Hvetemel", "Egg", "Vann", "Tomatsaus", "Mozzarellaost", "Cheddar", "Bacon"], 139);
+let veganer = new Pizza("Veganer Spesial", ["Nymalt Hvetemel", "Høyfjellsvann", "Økologisk Tomatsaus", "Nyklipt Gress", "Vegansk Ost", "Cherry Tomater"], 139);
+let biffOgSopp = new Pizza("Biff & Sopp", ["Hvetemel", "Egg", "Vann", "Tomatsaus", "Mozzarellaost", "Biff av storfe", "Kantarell", "Sjampinjong"], 129);
+let bbqChicken = new Pizza("BBQ Chicken", ["Hvetemel", "Egg", "Vann", "BBQ Saus", "Mozzarellaost", "Kylling"], 139);
+let sjokolade = new Pizza("Sjokolade Spesial", ["Digestivekjeks", "Egg", "Vann", "Nutella", "Marshmallows", "Nonstop", "Pisket Krem"], 139);
+
+// Drikker
