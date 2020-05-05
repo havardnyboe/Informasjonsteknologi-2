f0f9938a3eee0ea74d259fc5efb0cd1f7457cf57
Håvard Nybøe <havard.nyboe@gmail.com>
2020-05-05 14:59:27 +0200

    Create script.js

---
+++ 2019/35/Variabler del 4 - Oppgave/script.js
@@ -0,0 +1,17 @@
+console.log("Modulo gir resten av et divisjonsstykke, tegnet for modulo er %");
+console.log("Eks: 15%7 =");
+console.log(15%7);
+
+console.log("Inkrementeringsoperatoren legger til verdien 1 til et tall, tegnet for inkrementeringsoperatoren er ++");
+console.log("Eks: var tall1 = 2;");
+console.log("tall1++;");
+var tall1 = 2;
+tall1++;
+console.log(tall1);
+
+console.log("Dekrementeringsoperatoren trekker fra verdien 1 til et tall, tegnet for dekrementeringsoperatoren er --");
+console.log("Eks: var tall2 = 3;");
+console.log("tall2--;")
+var tall2 = 3;
+tall2--;
+console.log(tall2);
